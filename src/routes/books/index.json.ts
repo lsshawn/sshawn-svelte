67b6803b233f7d43e80918d4a19d80cfc557be10
ss <coffee.or.tea88@gmail.com>
2022-06-27 01:43:48 +0800

    load books slug

---
+++ src/routes/books/index.json.ts
@@ -0,0 +1,15 @@
+export async function get() {
+ const files = await import.meta.glob('../../content/books/*.{svx,md}');
+
+ const books = await Promise.all(
+   Object.keys(files).map(async (path) => {
+     const slug = path
+     const {metadata} = await files[path]();
+     return { slug, metadata }
+   })
+ );
+  return {
+    status: 200,
+    body: {books}
+  }
+}
---
+++ src/routes/books/index.svelte
@@ -0,0 +1,30 @@
+<script context="module">
+    // fetch data. Can run on server or client side.
+    export async function load({ fetch }) {
+        const res = await fetch('/books.json'); // context.fetch()
+        const { books } = await res.json();
+
+        if (res.ok) {
+            return {
+                props: {
+                    books
+                }
+            };
+        }
+
+        return {
+            status: res.status,
+            error: new Error('Could not fetch guides')
+        };
+    }
+</script>
+
+<script>
+    export let books;
+</script>
+
+<h1 class="text-center">Book Notes</h1>
+
+{#each books as book}
+    <div>{book.slug}</div>
+{/each}
