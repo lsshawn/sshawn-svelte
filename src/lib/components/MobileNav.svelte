78dc8aa2a0576265c686157e6ecb20c80fb1f83b
ss <coffee.or.tea88@gmail.com>
2022-07-20 17:00:46 +0800

    remove animation in bottom nav

--- src/lib/components/MobileNav.svelte
+++ src/lib/components/MobileNav.svelte
@@ -8,77 +8,75 @@
     $: basePath = $page.url.pathname.split('/')[1];
 </script>
 
+<!--
 <div class="tab-nav-container visible lg:hidden">
-    <a href="/" sveltekit:prefetch>
-        <div class="tab" class:active={basePath === ''}>
-            <Home />
-            <p>Home</p>
-        </div>
-    </a>
+</div>
+  -->
 
-    <a href="/now" sveltekit:prefetch>
-        <div class="tab" class:active={basePath === 'now'}>
-            <Now />
-            <p>Now</p>
-        </div>
-    </a>
+<div class="w-full h-screen">
+    <section
+        id="bottom-navigation"
+        class="block fixed inset-x-0 bottom-0 z-10 bg-white shadow"
+    >
+        <div id="tabs" class="flex justify-between">
+            <a
+                href="/"
+                sveltekit:prefetch
+                class="w-full hover:text-primary focus:text-primary inline-block justify-center text-center"
+                class:active={basePath === ''}
+            >
+                <div class="tab">
+                    <Home />
+                </div>
+                <span class="tab tab-explore block">Explore</span>
+            </a>
 
-    <a href="/books" sveltekit:prefetch>
-        <div class="tab" class:active={basePath === 'books'}>
-            <Book />
-            <p>Books</p>
-        </div>
-    </a>
+            <a
+                href="/now"
+                sveltekit:prefetch
+                class="w-full hover:text-primary focus:text-primary inline-block justify-center text-center"
+                class:active={basePath === 'now'}
+            >
+                <div class="tab">
+                    <Now />
+                </div>
+                <span class="tab tab-explore block ">Now</span>
+            </a>
 
-    <a href="/about" sveltekit:prefetch>
-        <div class="tab" class:active={basePath === 'about'}>
-            <About />
-            <p>About</p>
+            <a
+                href="/books"
+                sveltekit:prefetch
+                class="w-full hover:text-primary focus:text-primary inline-block justify-center text-center"
+                class:active={basePath === 'books'}
+            >
+                <div class="tab">
+                    <Book />
+                </div>
+                <span class="tab tab-explore block ">Books</span>
+            </a>
+
+            <a
+                href="/about"
+                sveltekit:prefetch
+                class="w-full hover:text-primary focus:text-primary inline-block justify-center text-center"
+                class:active={basePath === 'about'}
+            >
+                <div class="tab">
+                    <About />
+                </div>
+                <span class="tab tab-explore block ">Books</span>
+            </a>
         </div>
-    </a>
+    </section>
 </div>
 
 <style lang="scss">
-    .tab-nav-container {
-        display: flex;
-        padding: 10px;
-        justify-content: space-between;
-        align-items: center;
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-        height: 62px;
-        background-color: #ffffff;
-        box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2),
-            0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
-    }
-
-    .tab {
-        border-radius: 50px;
-        cursor: pointer;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding: 0 10px;
-        margin: 0 5px;
-        transition: background 0.4s linear;
-        height: 40px;
-        min-width: 20vw;
-
-        p {
-            font-weight: bold;
-            overflow: hidden;
-            max-width: 0;
-        }
-    }
-
-    .tab.active {
+    a.active {
         background-color: #f9a825;
-        color: #2e2e2e;
 
-        p {
-            max-width: 100px;
-            transition: max-width 0.4s linear;
+        span {
+            color: #111;
+            font-weight: bold;
         }
     }
 </style>
