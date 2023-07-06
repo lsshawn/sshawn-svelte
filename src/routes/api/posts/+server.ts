10c31fb7d01cb5ddde1109971b93c9a5c74517a9
sshawn <coffee.or.tea88@gmail.com>
2023-07-06 15:10:55 +0800

    eager:true is required

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,37 +1,25 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
-import { browser } from '$app/environment';
 import type { Post } from '$lib/types';
 
 async function getPosts(page = 1, limit = 10) {
-	const paths = import.meta.glob('/src/posts/*.md');
+	console.log('LS -> src/routes/api/posts/+server.ts:5 -> page: ', page);
+	const paths = import.meta.glob('/src/posts/*.md', { eager: true });
 
 	const startIndex = (page - 1) * limit;
 
 	const posts: Post[] = [];
 	for (const path in paths) {
-		console.log('LS -> src/routes/api/posts/+server.ts:29 -> path: ', path);
+		const file = paths[path];
 		const slug = path.split('/').at(-1)?.replace('.md', '');
 
-		const filePath = new URL(path, import.meta.url).pathname; // Get the full file path
-		console.log(
-			'LS -> src/routes/api/posts/+server.ts:15 -> filePath: ',
-			filePath,
-		);
-
-		const fileData = await import(filePath);
-		const metadata = fileData.metadata;
-
-		if (metadata && slug) {
-			if (metadata.unlisted || metadata.isPrivate) continue;
-			const meta = metadata as Omit<Post, 'slug'>;
-			const content = await (await fileData['default']).render()['html'];
+		if (file && typeof file === 'object' && 'metadata' in file && slug) {
+			const meta = file.metadata as Omit<Post, 'slug'>;
 			const post = {
 				meta,
 				slug,
-				content,
+				content: file['default'].render()['html'],
 			} satisfies Post;
-
-			posts.push(post);
+			if (!post.meta.unlisted && !post.meta.isPrivate) posts.push(post);
 		}
 	}
 
@@ -53,10 +41,8 @@ async function getPosts(page = 1, limit = 10) {
 }
 
 export async function GET({ url }: RequestEvent) {
-	const page = browser ? parseInt(url.searchParams.get('page') ?? '1', 10) : 1;
-	const limit = browser
-		? parseInt(url.searchParams.get('limit') ?? '10', 10)
-		: 10;
+	const page = parseInt(url.searchParams.get('page') ?? '1', 10);
+	const limit = parseInt(url.searchParams.get('limit') ?? '10', 10);
 	const posts = await getPosts(page, limit);
 	return json(posts);
 }
--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -1,13 +1,14 @@
-import * as config from '$lib/config'
-import type { Post } from '$lib/types'
+import * as config from '$lib/config';
+import type { Post } from '$lib/types';
 
-export const prerender = true
+export const prerender = true;
 
 export async function GET({ fetch }) {
-	const response = await fetch('api/posts')
-	const posts: Post[] = await response.json()
+	const response = await fetch('api/posts');
+	const data: PaginatedResponse = await response.json();
+	const posts = data.data;
 
-	const headers = { 'Content-Type': 'application/xml' }
+	const headers = { 'Content-Type': 'application/xml' };
 
 	const xml = `
 		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
@@ -26,12 +27,12 @@ export async function GET({ fetch }) {
 							<guid isPermaLink="true">${config.url}/${post.slug}</guid>
 							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
 						</item>
-					`
+					`,
 					)
 					.join('')}
 			</channel>
 		</rss>
-	`.trim()
+	`.trim();
 
-	return new Response(xml, { headers })
+	return new Response(xml, { headers });
 }
